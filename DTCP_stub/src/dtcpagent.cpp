ca1de1078391836fb6ffbc02a947fb4b530db669
Jim Lawton <jim.lawton@s3group.com>
2014-12-08 22:47:12 +0000

    RDKCMF-687  Update from Comcast source branch master.
    Change-Id: I97e96bac904071e6feb02674172bf973d763a61b
    Source: https://gerrit.teamccp.com/rdk/components/generic/tdk/generic
    SHA1: f1f382fe45e59382b6cf9203eefc96e96ee13944

--- DTCP_stub/src/dtcpagent.cpp
+++ DTCP_stub/src/dtcpagent.cpp
@@ -15,11 +15,12 @@
 #include <sstream>
 #include <fstream>
 #include <exception>
-static int dtcpinitialized = 0;
 
+#define RESULT_LEN	100
 
+#if 0
 /*************************************************************************
-Function name : searchPattern
+Function name : getIpAddress
 
 Arguments     : file path
 
@@ -27,28 +28,46 @@ return       : return string
 
 Description   : used to check  the ipaddress in the file.
 ***************************************************************************/
-string searchPattern(const char* filepath)
+std::string getIpAddress()
 {
-    string line;
-    string retString;
-    ifstream in(filepath);
-    DEBUG_PRINT(DEBUG_LOG,"\nInside the search pattern file name is %s \n",filepath);
+    char *cmd = "ifconfig eth1 | grep inet | tr -s ' ' | cut -d ' ' -f3 | sed -e 's/addr://g'";
+    char ipAddress[128] = {'\0'};
+    FILE *ptr;
 
-    std::stringstream buffer;
-    buffer<<in.rdbuf();	
-    string test = buffer.str();
-    DEBUG_PRINT(DEBUG_LOG,"\n content of the file is %s \n",(char*)test.c_str());
-    
-    return test;
-   
+    if ((ptr = popen(cmd, "r")) != NULL) {
+        while (fgets(ipAddress, 128, ptr) != NULL)
+            DEBUG_PRINT(DEBUG_LOG,"IP Address is %s \n",ipAddress);
+        pclose(ptr);
+    }
+
+    return std::string(ipAddress);
+}
+#endif
+
+dtcp_result_t createDTCPSinkSession(DTCP_SESSION_HANDLE *handle)
+{
+    char* srcIpAddress = (char*)"127.0.0.1";
+    int srcIpPort = 5000;
+    int maxPacketSize = 4096;
+    dtcp_result_t returnCode = DTCPMgrCreateSinkSession(srcIpAddress, srcIpPort, false, maxPacketSize, handle);
+    DEBUG_PRINT(DEBUG_LOG, "%s(): returnCode(%d)\n",__FUNCTION__,(int)returnCode);
+    return returnCode;
 }
 
-
-
+dtcp_result_t createDTCPSourceSession(DTCP_SESSION_HANDLE *handle)
+{
+    char* sinkIpAddress = (char*)"0.0.0.0";
+    int keyLabel = 0;
+    int pcpPacketSize = 0;
+    int maxPacketSize = 4096;
+    dtcp_result_t returnCode = DTCPMgrCreateSourceSession(sinkIpAddress,keyLabel,pcpPacketSize,maxPacketSize,handle);
+    DEBUG_PRINT(DEBUG_LOG, "%s(): returnCode(%d)\n",__FUNCTION__,(int)returnCode);
+    return returnCode;
+}
 
 DTCPAgent::DTCPAgent()
 {
-        DEBUG_PRINT(DEBUG_LOG, "DTCPAgent Initialized\n");
+    DEBUG_PRINT(DEBUG_LOG, "DTCPAgent Initialized\n");
 }
 
 /**************************************************************************
@@ -61,10 +80,10 @@ Description   : Registering all the wrapper functions with the agent for using t
 
 bool DTCPAgent::initialize(IN const char* szVersion,IN RDKTestAgent *ptrAgentObj)
 {
-        DEBUG_PRINT(DEBUG_ERROR, "DTCPAgent Initialization\n");
-	ptrAgentObj->RegisterMethod(*this,&DTCPAgent::DTCPAgent_Init, "TestMgr_DTCPAgent_Init");
+    DEBUG_PRINT(DEBUG_ERROR, "DTCPAgent Initialization\n");
+    ptrAgentObj->RegisterMethod(*this,&DTCPAgent::DTCPAgent_Init, "TestMgr_DTCPAgent_Init");
 
-        return TEST_SUCCESS;
+    return TEST_SUCCESS;
 }
 
 /***************************************************************************
@@ -76,12 +95,7 @@ bool DTCPAgent::initialize(IN const char* szVersion,IN RDKTestAgent *ptrAgentObj
 
 std::string DTCPAgent::testmodulepre_requisites()
 {
-	DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent testmodule pre_requisites --> Entry\n");
-
-        system ("ifconfig eth1 | grep inet | tr -s ' ' | cut -d ' ' -f3 | sed -e 's/addr://g' > /opt/TDK/dtcptestIPaddress.txt");
-     
-	DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent testmodule pre_requisites --> Exit\n");
-        return "SUCCESS";
+    return "SUCCESS";
 }
 
 /***************************************************************************
@@ -93,11 +107,7 @@ std::string DTCPAgent::testmodulepre_requisites()
 
 bool DTCPAgent::testmodulepost_requisites()
 {
-	DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent testmodule post_requisites --> Entry\n");
-
-	DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent testmodule post requisites --> Exit");
-
-       	return TEST_SUCCESS;
+    return TEST_SUCCESS;
 }
 
 
@@ -110,164 +120,163 @@ Description   : Receives the request from Test Manager to initialize dtcp module
 **************************************************************************/
 bool DTCPAgent::DTCPAgent_Init(IN const Json::Value& req, OUT Json::Value& response)
 {
-        DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent_Init --->Entry\n");
+    DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent_Init --->Entry\n");
 
-        string functionName = req["funcName"].asString();
-        dtcp_result_t returnType;
-        int ret=1;
-        string resultdetails;
-	DEBUG_PRINT(DEBUG_LOG, "Recieved function name %s \n", functionName.c_str());
-        char outputJsonpath[64]= {'\0'};
-        sprintf(outputJsonpath, "%s", "/opt/TDK/dtcptestIPaddress.txt");
-        string ipaddress ;
-        ipaddress= searchPattern(outputJsonpath);
+    string functionName = req["funcName"].asString();
+    dtcp_result_t returnCode = DTCP_SUCCESS;
+    char resultDetails[RESULT_LEN] = {'\0'};
+    DEBUG_PRINT(DEBUG_LOG,"Received function name: %s\n", functionName.c_str());
 
-	try
-	{
-	    	if (functionName.compare("DTCPMgrInitialize")==0)
-       		{
-                  if (dtcpinitialized == 0)
-        	{
-                	printf("initialized Manager\n");
-	                dtcpinitialized = 1;
-        	}
-        	else
-        	{	
-                	printf("Already initialized\n");
-        	}
-        	returnType = DTCPMgrInitialize();
-		}
-			
-	    	if (functionName.compare("DTCPMgrStartSource")==0)
-	       	{
-	        char ifname[64] = {'\0'};
-                string par1= req["param1"].asString();
-	        sprintf(ifname,"%s",par1.c_str());
-                if (ifname == NULL) 
-	        sprintf(ifname,"%s",ipaddress.c_str());
-                if(dtcpinitialized != 1)
-		DTCPMgrInitialize();
-		int portNo = req["param2"].asInt();
-		returnType = DTCPMgrStartSource(ifname,portNo);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-		
-		DTCPMgrStopSource();
-		}
-	   	
-		if (functionName.compare("DTCPMgrStopSource")==0)
-	       	{ 
-			if (dtcpinitialized == 1)
-        		{
-	                printf("stopped DTCP Manager\n");
-	                 dtcpinitialized = 0;
-			}
-       			 else
-        		{
-                		printf ("Already stopped\n");
-        		}
-
-	         returnType = DTCPMgrStopSource();
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-		}
-	    	
-		if (functionName.compare("DTCPMgrCreateSourceSession")==0)
-	       	{
-	        char ipname[64] = {'\0'};
-		string sinkip = req["param1"].asString();
-	        sprintf(ipname, "%s",sinkip.c_str());
-	        int keylabel=req["param2"].asInt();
-	        int pcppacketsz = req["param3"].asInt();
-	        int maxpacketsz = req["param4"].asInt();
-		DTCP_SESSION_HANDLE *handle =new DTCP_SESSION_HANDLE ;
-		returnType = DTCPMgrCreateSourceSession(ipname,keylabel,pcppacketsz,maxpacketsz,handle);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-                }
-
-	    	if (functionName.compare("DTCPMgrCreateSinkSession")==0)
-	       	{
-	        char ipname[64] = {'\0'};
-		string srcip =req["param1"].asString();
-	        sprintf(ipname, "%s",srcip.c_str());
-	        int portNo=req["param2"].asInt();
-		DEBUG_PRINT(DEBUG_LOG, "Before calling the  p3\n");
-	        BOOLEAN p3=(BOOLEAN)req["param3"].asInt();
-	        int maxpacketsz = req["param4"].asInt();
-		DTCP_SESSION_HANDLE *handle =new DTCP_SESSION_HANDLE;
-		returnType = DTCPMgrCreateSinkSession(ipname,portNo,p3,maxpacketsz,handle);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-		delete handle;
- 		}
-		
-		if (functionName.compare("DTCPMgrProcessPacket")==0)
-	       	{
-		DTCP_SESSION_HANDLE session;
-	        //session= (DTCP_SESSION_HANDLE)req["param1"].asInt();
-		DTCPIP_Packet *packet=new DTCPIP_Packet;	 	
-		returnType = DTCPMgrProcessPacket( session, packet);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-		}
-		
-		if (functionName.compare("DTCPMgrReleasePacket")==0)
-	       	{
-		DTCPIP_Packet *packet=new DTCPIP_Packet;	 	
-		returnType = DTCPMgrReleasePacket( packet);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-                delete packet;
-		}
-
-		if (functionName.compare("DTCPMgrDeleteDTCPSession")==0)
-	       	{
-		DTCP_SESSION_HANDLE session;
-		returnType = DTCPMgrDeleteDTCPSession(session);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-		}
-
-		if (functionName.compare("DTCPMgrGetNumSessions")==0)
-	       	{
-		DTCPDeviceType deviceType;
-		ret = DTCPMgrGetNumSessions(deviceType);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),ret);
-		}
-
-		if (functionName.compare("DTCPMgrGetSessionInfo")==0)
-	       	{
-		DTCP_SESSION_HANDLE handle;
-	        //handle= (DTCP_SESSION_HANDLE)req["param1"].asInt();
-		DTCPIP_Session *session=new DTCPIP_Session;	 	
-		returnType = DTCPMgrGetSessionInfo(handle,session);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-                delete session;
-		}
-
-		if (functionName.compare("DTCPMgrSetLogLevel")==0)
-	       	{
-		int level= req["param2"].asInt();
-		returnType = DTCPMgrSetLogLevel(level);
-		DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnType);
-		}
-
-
-	}
-	catch(exception &e)
+    try
+    {
+        if (functionName.compare("DTCPMgrInitialize")==0)
         {
-                DEBUG_PRINT(DEBUG_ERROR,"Exception occured during function call ");
-                DEBUG_PRINT(DEBUG_LOG,"Exception catched is %s ", e.what() );
-                response["details"]= "FAILURE:Exception occured during function call "+req["funcName"].asString();
-       		response["result"]="FAILURE";
-		return TEST_FAILURE;
+            returnCode = DTCPMgrInitialize();
+            DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
         }
+        else if (functionName.compare("DTCPMgrStartSource")==0)
+        {
+            string ifname = req["param1"].asString();
+            int portNo = req["param2"].asInt();
 
-               
-	DEBUG_PRINT(DEBUG_LOG, "Before updating the details function %s return value %d \n",functionName.c_str(),(int)returnType);
-	char resval[10] = {'\0'};
-	if(ret !=1)        
-	sprintf(resval, "%d",(int)returnType);
-	else
-	sprintf(resval, "%d",ret);
-        response["details"]= resval;
-     	DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent_Init -->Exit\n");
-       	response["result"]="SUCCESS";
-       	return TEST_SUCCESS;
+            DEBUG_PRINT(DEBUG_LOG,"Input [ifname:%s portNo:%d]\n",ifname.c_str(),portNo);
+            returnCode = DTCPMgrStartSource(&ifname[0],portNo);
+            DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+        }
+        else if (functionName.compare("DTCPMgrStopSource")==0)
+        {
+            returnCode = DTCPMgrStopSource();
+            DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+        }
+        else if (functionName.compare("DTCPMgrCreateSourceSession")==0)
+        {
+            string sinkip = req["param1"].asString();
+            int keylabel = req["param2"].asInt();
+            int pcppacketsz = req["param3"].asInt();
+            int maxpacketsz = req["param4"].asInt();
+
+            DEBUG_PRINT(DEBUG_LOG,"Input [SinkIp:%s keyLabel:%d pcpPacketSize:%d maxPacketSize:%d]\n",sinkip.c_str(),keylabel,pcppacketsz,maxpacketsz);
+            DTCP_SESSION_HANDLE *handle = new DTCP_SESSION_HANDLE;
+            returnCode = DTCPMgrCreateSourceSession(&sinkip[0],keylabel,pcppacketsz,maxpacketsz,handle);
+            DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+            delete handle;
+        }
+        else if (functionName.compare("DTCPMgrCreateSinkSession")==0)
+        {
+            string srcip = req["param1"].asString();
+            int portNo = req["param2"].asInt();
+            bool uniqueKey = req["param3"].asInt();
+            int maxpacketsz = req["param4"].asInt();
+
+            DEBUG_PRINT(DEBUG_LOG,"Input [SrcIp:%s srcPort:%d uniqueKey:%d maxPacketSize:%d]\n",srcip.c_str(),portNo,uniqueKey,maxpacketsz);
+            DTCP_SESSION_HANDLE *handle =new DTCP_SESSION_HANDLE;
+            returnCode = DTCPMgrCreateSinkSession(&srcip[0],portNo,uniqueKey,maxpacketsz,handle);
+            DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+            delete handle;
+        }
+        else if (functionName.compare("DTCPMgrProcessPacket")==0)
+        {
+            //Create DTCPSinkSession
+            DTCP_SESSION_HANDLE pDtcpSession = 0;
+            returnCode = createDTCPSinkSession(&pDtcpSession);
+            if(DTCP_SUCCESS == returnCode)
+            {
+                DTCPIP_Packet *packet=new DTCPIP_Packet;
+                returnCode = DTCPMgrProcessPacket(pDtcpSession, packet);
+                DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+                delete packet;
+            }
+            else
+            {
+                DEBUG_PRINT(DEBUG_ERROR, "Failed to create session for DTCPMgrProcessPacket (%d)\n",(int)returnCode);
+            }
+        }
+        else if (functionName.compare("DTCPMgrReleasePacket")==0)
+        {
+            DTCPIP_Packet *packet=new DTCPIP_Packet;
+            returnCode = DTCPMgrReleasePacket( packet);
+            DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+            delete packet;
+        }
+        else if (functionName.compare("DTCPMgrDeleteDTCPSession")==0)
+        {
+            //Create DTCPSinkSession
+            DTCP_SESSION_HANDLE pDtcpSession = 0;
+            returnCode = createDTCPSinkSession(&pDtcpSession);
+            if(DTCP_SUCCESS == returnCode)
+            {
+                returnCode = DTCPMgrDeleteDTCPSession(pDtcpSession);
+                DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+            }
+            else
+            {
+                DEBUG_PRINT(DEBUG_ERROR, "Failed to create session for DTCPMgrDeleteDTCPSession (Error:%d)\n",(int)returnCode);
+            }
+        }
+        else if (functionName.compare("DTCPMgrGetNumSessions")==0)
+        {
+            int iDeviceType = req["param2"].asInt();
+            if (((DTCPDeviceType)iDeviceType < DTCP_SOURCE) or ((DTCPDeviceType)iDeviceType > DTCP_UNKNOWN))
+                iDeviceType = (DTCPDeviceType)DTCP_UNKNOWN;
+
+            DEBUG_PRINT(DEBUG_LOG,"Input [DTCPDeviceType:%d]\n",iDeviceType);
+            int numSessions = DTCPMgrGetNumSessions((DTCPDeviceType)iDeviceType);
+            DEBUG_PRINT(DEBUG_LOG,"function %s return value %d numSessions %d \n",functionName.c_str(),(int)returnCode,numSessions);
+        }
+        else if (functionName.compare("DTCPMgrGetSessionInfo")==0)
+        {
+            //Create DTCPSinkSession
+            DTCP_SESSION_HANDLE handle = 0;
+            returnCode = createDTCPSinkSession(&handle);
+            if(DTCP_SUCCESS == returnCode)
+            {
+                DTCPIP_Session *session=new DTCPIP_Session;
+                returnCode = DTCPMgrGetSessionInfo(handle,session);
+                delete session;
+                DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+            }
+            else
+            {
+                DEBUG_PRINT(DEBUG_ERROR, "Failed to create handle for DTCPMgrGetSessionInfo (Error:%d)\n",(int)returnCode);
+            }
+        }
+        else if (functionName.compare("DTCPMgrSetLogLevel")==0)
+        {
+            int level= req["param2"].asInt();
+
+            DEBUG_PRINT(DEBUG_LOG,"Input [LogLevel:%d]\n",level);
+            returnCode = DTCPMgrSetLogLevel(level);
+            DEBUG_PRINT(DEBUG_LOG, "function %s return value %d \n",functionName.c_str(),(int)returnCode);
+        }
+        else
+        {
+            DEBUG_PRINT(DEBUG_ERROR,"Unsupported function call\n");
+            response["details"]= "Unsupported function call";
+            response["result"]="FAILURE";
+            DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent_Init -->Exit\n");
+            return TEST_FAILURE;
+        }
+    }
+    catch(exception &e)
+    {
+        DEBUG_PRINT(DEBUG_ERROR,"Exception occured during function call\n");
+        DEBUG_PRINT(DEBUG_LOG,"Exception caught is %s\n", e.what() );
+        response["details"]= "Exception occured during function call";
+        response["result"]="FAILURE";
+        return TEST_FAILURE;
+    }
+
+    // Update details and result in response msg
+    sprintf(resultDetails,"Function:%s ReturnCode:%d",functionName.c_str(),(int)returnCode);
+    response["details"]= resultDetails;
+
+    if (DTCP_SUCCESS != returnCode)
+        response["result"]="FAILURE";
+    else
+        response["result"]="SUCCESS";
+
+    DEBUG_PRINT(DEBUG_TRACE, "DTCPAgent_Init -->Exit\n");
+    return TEST_SUCCESS;
 }
 /**************************************************************************
 Function Name   : CreateObject
@@ -279,7 +288,7 @@ Description     : This function is used to create a new object of the class "DTC
 
 extern "C" DTCPAgent* CreateObject()
 {
-        return new DTCPAgent();
+    return new DTCPAgent();
 }
 
 /**************************************************************************
@@ -292,14 +301,14 @@ Description     : This function will be used to the close things cleanly.
 
 bool DTCPAgent::cleanup(IN const char* szVersion, IN RDKTestAgent *ptrAgentObj)
 {
-        DEBUG_PRINT(DEBUG_TRACE, "cleaningup\n");
-        if(NULL == ptrAgentObj)
-        {
-                return TEST_FAILURE;
-        }
-	ptrAgentObj->UnregisterMethod("TestMgr_DTCPAgent_Init");
+    DEBUG_PRINT(DEBUG_TRACE, "cleaningup\n");
+    if(NULL == ptrAgentObj)
+    {
+        return TEST_FAILURE;
+    }
+    ptrAgentObj->UnregisterMethod("TestMgr_DTCPAgent_Init");
 
-        return TEST_SUCCESS;
+    return TEST_SUCCESS;
 }
 /**************************************************************************
 Function Name : DestroyObject
@@ -310,6 +319,6 @@ Description   : This function will be used to destory the DTCPAgent object.
 **************************************************************************/
 extern "C" void DestroyObject(DTCPAgent *stubobj)
 {
-        DEBUG_PRINT(DEBUG_LOG, "Destroying DTCPAgent Agent object\n");
-        delete stubobj;
+    DEBUG_PRINT(DEBUG_LOG, "Destroying DTCPAgent Agent object\n");
+    delete stubobj;
 }
