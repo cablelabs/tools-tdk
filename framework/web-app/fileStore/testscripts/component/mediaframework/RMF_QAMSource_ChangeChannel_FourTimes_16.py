f89b842a7acd9701ac40587b7ba44698a3c325c5
Jim Lawton <jim.lawton@s3group.com>
2015-05-15 14:35:16 +0100

    RDKCMF-1414  Update from Comcast source (master).
     * Fixing the script issues in the Mediaframework test module
    
    Change-Id: Ie3bbe908c565b14615b261d0b966b3b7676cd4a6
    Source: https://gerrit.teamccp.com/rdk/components/generic/tdk/generic
    SHA1: 8f9306bf9b23a5a67401fdc306fbdb43f19633e3

--- framework/web-app/fileStore/testscripts/component/mediaframework/RMF_QAMSource_ChangeChannel_FourTimes_16.py
+++ framework/web-app/fileStore/testscripts/component/mediaframework/RMF_QAMSource_ChangeChannel_FourTimes_16.py
@@ -34,8 +34,6 @@ Test Case Type: Positive.</synopsis>
     <!--  -->
     <box_type>Emulator-HYB</box_type>
     <!--  -->
-    <box_type>Terminal-RNG</box_type>
-    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDK2.0</rdk_version>
