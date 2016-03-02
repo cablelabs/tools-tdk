8b913002da0b3bd41b7daa66793d6d04cd76b4b4
Jim Lawton <jim.lawton@accenture.com>
2016-03-02 08:59:36 +0000

    RDKCMF-3100  Update from Comcast source (master).
     * Report Errors for Lost Server Connections
    
    Change-Id: Idd15d45f7fb04918a1a06984a2f80649b7d2c8af
    Source: https://gerrit.teamccp.com/rdk/components/generic/tdk/generic
    SHA1: adde2f94138cd6f05f216493f123a50b35b5c914

---
+++ framework/web-app/fileStore/testscripts/component/recorder/Recorder_RMF_Configure_WrongRWSUrl_Rmfconfig_255.py
@@ -0,0 +1,175 @@
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>2</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>Recorder_RMF_Configure_WrongRWSUrl_Rmfconfig_255</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>Recorder_SendRequest</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>CT_Recoder_DVR_Protocol_255 - Check the error code after Bad RWS URL configured in the rmfconfig.ini</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>60</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Hybrid-1</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <script_tags />
+</xml>
+'''
+#use tdklib library,which provides a wrapper for tdk test case script
+import tdklib;
+import recorderlib
+from random import randint
+from time import sleep
+
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding Box IP and port while executing script
+ip = <ipaddress>
+port = <port>
+
+#Test component to be tested
+recObj = tdklib.TDKScriptingLibrary("Recorder","2.0");
+recObj.configureTestCase(ip,port,'Recorder_RMF_Configure_WrongRWSUrl_Rmfconfig_255');
+#Get the result of connection with test component and STB
+recLoadStatus = recObj.getLoadModuleResult();
+print "Recorder module loading status : %s" %recLoadStatus;
+#Set the module loading status
+recObj.setLoadModuleStatus(recLoadStatus);
+
+obj = tdklib.TDKScriptingLibrary("mediaframework","2.0");
+obj.configureTestCase(ip,port,'Recorder_RMF_Configure_WrongRWSUrl_Rmfconfig_255');
+MFLoadStatus = obj.getLoadModuleResult();
+print "MF module loading status : %s" %MFLoadStatus
+
+#Check for SUCCESS/FAILURE of Recorder module
+if "SUCCESS" in recLoadStatus.upper():
+
+	loadmoduledetails = recObj.getLoadModuleDetails();
+        if "REBOOT_REQUESTED" in loadmoduledetails:
+            recObj.initiateReboot();
+	    sleep(300);
+	    print "Sleeping to wait for the recoder to be up"
+
+        #To clear all the alternate URL's set for Servers
+        actResponse = recorderlib.callServerHandlerWithType('clearAlternateURL','LPServer',ip);
+        actResponse = recorderlib.callServerHandlerWithType('clearAlternateURL','RWSServer',ip);
+        actResponse = recorderlib.callServerHandlerWithType('clearAlternateURL','RWSStatus',ip);
+        print "Cleared all alternate URL's set in Servers";
+
+        #Primitive test case which associated to this script
+        rmfConfObj = recObj.createTestStep('Recorder_SetValuesInRmfconfig');
+        expectedResult="SUCCESS";
+        #Set 2 parameters
+        Keyword="FEATURE.RWS.GET.URL";
+        rmfConfObj.addParameter("Keyword",Keyword);
+        Value="http://96.114.220.106:8080/DVRSimulator/wrongRWS";
+        rmfConfObj.addParameter("Value",Value);
+        #Execute the test case in STB
+        rmfConfObj.executeTestCase(expectedResult);
+        #Get the actual result and details of execution
+        result = rmfConfObj.getResult();
+        details1 = rmfConfObj.getResultDetails();
+        print result,","," ",details1
+        if "FAILURE" in result:
+                print "Failed to change the RWS Url"
+                rmfConfObj.setResultStatus("FAILURE");
+                recObj.unloadModule("Recorder");
+                exit();
+        print "Changed the RWS Url"
+        rmfConfObj.setResultStatus("SUCCESS");
+        sleep(20);      
+ 
+        #To clear the ocapri log
+        tdkTestObj1 = recObj.createTestStep('Recorder_clearOcapri_log');
+        tdkTestObj1.executeTestCase(expectedResult);
+        result = tdkTestObj1.getResult();
+        if "SUCCESS" in result:
+            tdkTestObj1.setResultStatus("SUCCESS");
+            print "Cleared the ocapri log ";
+        else:
+            tdkTestObj1.setResultStatus("FAILURE");
+            print "Ocapri log is not cleared ";
+
+        #unloading Recorder module
+        recObj.unloadModule("Recorder");
+        sleep(30);
+        #Reboot the STB
+        obj.initiateReboot();
+        print "Sleeping to wait for the recoder to be up"
+        sleep(300); 
+        #recObj.initiateReboot();
+   
+        #Test component to be tested
+        recObj = tdklib.TDKScriptingLibrary("Recorder","2.0");
+        recObj.configureTestCase(ip,port,'Recorder_RMF_Configure_WrongRWSUrl_Rmfconfig_255');
+        #Get the result of connection with test component and STB
+        recLoadStatus = recObj.getLoadModuleResult();
+        print "Recorder module loading status : %s" %recLoadStatus;
+        recObj.setLoadModuleStatus(recLoadStatus);
+   
+        tdkTestObj2=recObj.createTestStep('Recorder_checkOcapri_log');
+        pattern = "RDK-10028"
+        tdkTestObj2.addParameter("pattern",pattern);
+        tdkTestObj2.executeTestCase(expectedResult);  
+        result = tdkTestObj2.getResult();
+        details = tdkTestObj2.getResultDetails();
+        print result,",Details of log ",details
+        if "SUCCESS" in result:
+            tdkTestObj2.setResultStatus("SUCCESS");
+            print "Error Log RDK-10028 for RWS server connection lost is found ";
+        else:
+            tdkTestObj2.setResultStatus("SUCCESS");
+            print "Error Log RDK-10028 for RWS server connection lost is NOT found "; 
+       
+        rmfConfObj = recObj.createTestStep('Recorder_SetValuesInRmfconfig');
+        expectedResult="SUCCESS";
+        #Set 2 parameters
+        Keyword="FEATURE.RWS.GET.URL";
+        rmfConfObj.addParameter("Keyword",Keyword);
+        Value="http://96.114.220.106:8080/DVRSimulator/wrongRWS";
+        rmfConfObj.addParameter("Value",details1);
+        expectedResult="SUCCESS";
+        #Execute the test case in STB
+        rmfConfObj.executeTestCase(expectedResult);
+        #Get the actual result and details of execution
+        result = rmfConfObj.getResult();
+        details = rmfConfObj.getResultDetails();
+        print result,","," ",details
+        if "FAILURE" in result:
+                print "Failed to revert the RWS Url"
+                rmfConfObj.setResultStatus("FAILURE");
+                recObj.unloadModule("Recorder");
+                exit();
+        print "Reverted the RWS Url"
+        rmfConfObj.setResultStatus("SUCCESS");    
+      
+
+        #unloading Recorder module
+        recObj.unloadModule("Recorder");
+else:
+    print "Failed to load Recorder module";
+    #Set the module loading status
+    recObj.setLoadModuleStatus("FAILURE"); 
--- framework/web-app/fileStore/testscripts/component/recorder/Recorder_RMF_Disable_LongpollServer_During_Recording_Inline_242.py
+++ framework/web-app/fileStore/testscripts/component/recorder/Recorder_RMF_Disable_LongpollServer_During_Recording_Inline_242.py
@@ -3,49 +3,9 @@
 <xml>
   <id></id>
   <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
-  <version>3</version>
-  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
-  <name>Recorder_RMF_Disable_LongpollServer_During_Recording_241</name>
-  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
-  <primitive_test_id></primitive_test_id>
-  <!-- Do not change primitive_test_id if you are editing an existing script. -->
-  <primitive_test_name>Recorder_SendRequest</primitive_test_name>
-  <!--  -->
-  <primitive_test_version>1</primitive_test_version>
-  <!--  -->
-  <status>FREE</status>
-  <!--  -->
-  <synopsis>CT_Recorder_DVR_Protocol_241 - Recorder- To check whether recorder sends error or not for  the loss of connection with Long poll Server during recording operations</synopsis>
-  <!--  -->
-  <groups_id />
-  <!--  -->
-  <execution_time>30</execution_time>
-  <!--  -->
-  <long_duration>false</long_duration>
-  <!-- execution_time is the time out time for test execution -->
-  <remarks></remarks>
-  <!-- Reason for skipping the tests if marked to skip -->
-  <skip>false</skip>
-  <!--  -->
-  <box_types>
-    <box_type>Hybrid-1</box_type>
-    <!--  -->
-  </box_types>
-  <rdk_versions>
-    <rdk_version>RDK2.0</rdk_version>
-    <!--  -->
-  </rdk_versions>
-  <script_tags />
-</xml>
-'''
-'''
-<?xml version='1.0' encoding='utf-8'?>
-<xml>
-  <id></id>
-  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
   <version>2</version>
   <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
-  <name>Recorder_RMF_Configure_WrongLongpoll_Url</name>
+  <name>Recorder_RMF_Disable_LongpollServer_During_Recording_Inline_242</name>
   <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
   <primitive_test_id></primitive_test_id>
   <!-- Do not change primitive_test_id if you are editing an existing script. -->
@@ -55,11 +15,11 @@
   <!--  -->
   <status>FREE</status>
   <!--  -->
-  <synopsis></synopsis>
+  <synopsis>CT_Recorder_DVR_Protocol_242 - Recorder- To check whether recorder sends error or not for  the loss of connection with Long poll Server during recording operations</synopsis>
   <!--  -->
   <groups_id />
   <!--  -->
-  <execution_time>30</execution_time>
+  <execution_time>60</execution_time>
   <!--  -->
   <long_duration>false</long_duration>
   <!-- execution_time is the time out time for test execution -->
@@ -92,7 +52,7 @@ port = <port>
 
 #Test component to be tested
 recObj = tdklib.TDKScriptingLibrary("Recorder","2.0");
-recObj.configureTestCase(ip,port,'Recorder_RMF_Disable_LongpollServer_During_Recording_241');
+recObj.configureTestCase(ip,port,'Recorder_RMF_Disable_LongpollServer_During_Recording_Inline_242');
 #Get the result of connection with test component and STB
 recLoadStatus = recObj.getLoadModuleResult();
 print "Recorder module loading status : %s" %recLoadStatus;
@@ -140,7 +100,7 @@ if "SUCCESS" in recLoadStatus.upper():
         #Frame json message to schedule a recording
         jsonMsg = "{\"updateSchedule\":{\"requestId\":\""+requestID+"\",\"generationId\":\"TDK123\",\"dvrProtocolVersion\":\"7\",\"schedule\":[{\"recordingId\":\""+recordingID+"\",\"locator\":[\"ocap://"+ocapId+"\"],\"epoch\":"+now+",\"start\":"+startTime+",\"duration\":"+duration+",\"properties\":{\"requestedStart\":0,\"title\":\"Recording_"+recordingID+"\"},\"bitRate\":\"HIGH_BIT_RATE\",\"deletePriority\":\"P3\"}]}}";
 
-        actResponse = recorderlib.callServerHandlerWithMsg('updateMessage',jsonMsg,ip);
+        actResponse = recorderlib.callServerHandlerWithMsg('updateInlineMessage',jsonMsg,ip);
         sleep(10);
      
         #To disable the long poll server
