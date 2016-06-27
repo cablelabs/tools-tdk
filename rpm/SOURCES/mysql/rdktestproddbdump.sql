51e029dc808938f26cf2f99fe4d0c6db5ae672df
Jim Lawton <jim.lawton@accenture.com>
2016-06-27 06:12:32 +0100

    RDKCMF-3848  Update from Comcast source (master).
     * TDK TM Fix black duck violations
     * TDK TM Fix black duck violations
    
    Change-Id: I9893e84104a94e53037a9693b82f47ad9557d4d9
    Source: https://gerrit.teamccp.com/rdk/components/generic/tdk/generic
    SHA1: c5d24df954e28c3937c39e94edae521f64f1c82c

--- rpm/SOURCES/mysql/rdktestproddbdump.sql
+++ rpm/SOURCES/mysql/rdktestproddbdump.sql
@@ -1,3 +1,12 @@
+--  ============================================================================
+-- COMCAST C O N F I D E N T I A L AND PROPRIETARY
+-- ============================================================================
+-- This file (and its contents) are the intellectual property of Comcast.  It may
+-- not be used, copied, distributed or otherwise  disclosed in whole or in part
+-- without the express written permission of Comcast.
+-- ============================================================================
+-- Copyright (c) 2014 Comcast. All rights reserved.
+-- ===========================================================================
 -- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: rdktesttoolproddb
--- rpm/master_rpm.sh
+++ rpm/master_rpm.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#
+# ============================================================================
+# COMCAST C O N F I D E N T I A L AND PROPRIETARY
+# ============================================================================
+# This file (and its contents) are the intellectual property of Comcast.  It may
+# not be used, copied, distributed or otherwise  disclosed in whole or in part
+# without the express written permission of Comcast.
+# ============================================================================
+# Copyright (c) 2013 Comcast. All rights reserved.
+# ============================================================================
+#
 #File to install Java(JRE), Apache Tomcat, Mysql and TFTPY. It will upload the latest war to Tomcat also.
 
 
