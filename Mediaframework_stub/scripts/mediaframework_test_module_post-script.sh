b393f1c362c3d5e6a9a9a324746630c737ab1c32
sarveswaran <sarveswaran@tataelxsi.co.in>
2017-01-12 11:58:30 -0500

    RDKCMF-6326 Changing the platform related code as generic
    Reason for change: Minimizing TDK porting effort
    Test Procedure: Using TDK tool
    Risks: None
    Signed-off-by: sarveswaran <sarveswaran@tataelxsi.co.in>
    
    Change-Id: I9c500607e863267924b4881185a8d4a25f699e90

---
+++ Mediaframework_stub/scripts/mediaframework_test_module_post-script.sh
@@ -0,0 +1,47 @@
+# ============================================================================
+# RDK MANAGEMENT, LLC CONFIDENTIAL AND PROPRIETARY
+# ============================================================================
+# This file (and its contents) are the intellectual property of RDK Management, LLC.
+# It may not be used, copied, distributed or otherwise  disclosed in whole or in
+# part without the express written permission of RDK Management, LLC.
+# ============================================================================
+# Copyright (c) 2016 RDK Management, LLC. All rights reserved.
+# ============================================================================
+#
+RMFCONFIG_PATH=/etc
+TARGET_PATH=/opt
+LOGPATH=$TDK_PATH/logs
+LOGFILE=Mediaframework_testmodule_postreq_details.log
+#Create if log file not there
+mkdir -p $LOGPATH
+
+#removing old configuring status from the opt
+rm $LOGPATH/$LOGFILE
+
+#removing the rmfconfig.ini file from /opt/
+if rm $TARGET_PATH/rmfconfig.ini; then
+   echo "rmfconfig.ini removed from "$TARGET_PATH
+else
+   echo "Cant able to remove rmfconfig.ini"
+   touch $LOGPATH/$LOGFILE
+   echo "FAILURE<details>Not able to delete rmfconfig.ini" > $LOGPATH/$LOGFILE
+   exit 1
+fi
+
+#Restart the rmfstreamer,vodclient,podmanager & snmp
+process_name="rmf-streamer vod-service pod-service snmp-manager-service"
+for process in $process_name
+do
+	/etc/init.d/$process restart
+	if [ $? -eq 0 ]
+	then
+		echo $process "restarted"
+		touch $LOGPATH/$LOGFILE;
+		echo "SUCCESS" > $LOGPATH/$LOGFILE
+	else
+		echo $process "not able to restart"
+		touch $LOGPATH/$LOGFILE;
+		echo "FAILURE<details>Not able to restart" > $LOGPATH/$LOGFILE
+		
+	fi
+done
