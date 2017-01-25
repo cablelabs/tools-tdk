e20b84e386baba9574da86e4479942df6a8283f4
snampoothiri <Sreelal_Nampoothiri@cable.comcast.com>
2017-01-24 23:49:19 -0500

    RDKCMF-6328 TDK TM : New REST API getTMIPAddress
    Reason for change: New REST API getTMIPAddress
    Test Procedure: using TDK Test Manager
    Risks: None
    Signed-off-by: Sreelal_Nampoothiri@cable.comcast.com
    
    Change-Id: I32e18e91730134a72ee5017572c1e427c8769948

--- framework/grails-app/conf/com/comcast/rdk/SecurityFilters.groovy
+++ framework/grails-app/conf/com/comcast/rdk/SecurityFilters.groovy
@@ -26,7 +26,7 @@ class SecurityFilters {
     def publicActions = [     
 		user: ['changePassword','registerUser','saveUser'],
 		primitiveTest : ['getJson','getStreamDetails'],
-		execution : ['saveLoadModuleStatus','saveResultDetails','getDeviceStatusList','getDeviceStatus','thirdPartyJsonResult','thirdPartyTest','showResult','getDetailedTestResult','getClientPort','stopThirdPartyTestExecution','getAgentConsoleLog','getRealtimeDeviceStatus','getExecutionOutput','thirdPartySingleTestExecution','thirdPartyJsonPerformanceResult','getExecutionId','getImageName','getExecutionList', 'getDeviceImageName','thirdPartyMultipleScriptExecution','uploadLogs','getExecutionStatus','exportConsolidatedToExcel'],
+		execution : ['saveLoadModuleStatus','saveResultDetails','getDeviceStatusList','getDeviceStatus','thirdPartyJsonResult','thirdPartyTest','showResult','getDetailedTestResult','getClientPort','stopThirdPartyTestExecution','getAgentConsoleLog','getRealtimeDeviceStatus','getExecutionOutput','thirdPartySingleTestExecution','thirdPartyJsonPerformanceResult','getExecutionId','getImageName','getExecutionList', 'getDeviceImageName','thirdPartyMultipleScriptExecution','uploadLogs','getExecutionStatus','exportConsolidatedToExcel','getTMIPAddress'],
 		deviceGroup : ['uploadAgentBinaries','getDeviceList','deleteDeviceMethod','createNewDevice','uploadTclConfig'],
 		scriptGroup : ['getScriptsByScriptGroup','getScriptsByModule','getAllScriptGroups','deleteScriptGroup','createNewScriptGroup','verifyScriptGroup','getScriptTimeout'],
 		module : ['getModuleScriptTimeOut']
