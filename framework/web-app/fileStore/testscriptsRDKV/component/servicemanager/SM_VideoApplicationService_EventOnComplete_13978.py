e7d50249da9b6e8414ad88ec8154fc958806a9ae
smohanakumari <swapna_mohanakumari@comcast.com>
2017-04-25 04:43:00 +0000

    RDKCMF-6904 Automation for Video Application Events Service
    Reason for change: New Video Application Events Service added
    Test Procedure: Run Video Application Events Service scripts using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna.sm@tataelxsi.co.in>
    
    Change-Id: Ib79e27e8f024917d2b2be1ed2482a74925162f83

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/servicemanager/SM_VideoApplicationService_EventOnComplete_13978.py
@@ -0,0 +1,114 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2016 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>SM_VideoApplicationService_EventOnComplete_13978</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>SM_RunSMEvent_QtApp</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis></synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Hybrid-1</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id></test_case_id>
+    <test_objective></test_objective>
+    <test_type></test_type>
+    <test_setup></test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used></api_or_interface_used>
+    <input_parameters></input_parameters>
+    <automation_approch></automation_approch>
+    <except_output></except_output>
+    <priority></priority>
+    <test_stub_interface></test_stub_interface>
+    <test_script></test_script>
+    <skipped></skipped>
+    <release_version></release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib;
+import servicemanager;
+from tdklib import TDKScriptingLibrary;
+from time import sleep;
+
+#Test component to be tested
+smObj = tdklib.TDKScriptingLibrary("servicemanager","1.3");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+smObj.configureTestCase(ip,port,'SM_VideoApplicationService_EventOnComplete_13978');
+#Get the result of connection with test component and STB
+smLoadStatus = smObj.getLoadModuleResult();
+print "[SM LIB LOAD STATUS]  :  %s" %smLoadStatus;
+
+#Set the module loading status
+smObj.setLoadModuleStatus(smLoadStatus.upper());
+
+if "SUCCESS" in smLoadStatus.upper():
+        tdkTestObj = smObj.createTestStep('SM_RunSMEvent_QtApp');
+        service_name = "org.openrdk.videoApplicationEvents_1"
+        event_name = "onComplette"
+        event_param = 1
+        expectedresult = "SUCCESS";
+        ret = servicemanager.RunSMEvent(smObj, service_name, event_name, event_param);
+        if expectedresult in ret:
+                print "Successfully received AVInputActive event"
+        else:
+                print "QApp execution failed, AVInputActive event not received";
+
+        #Unload the modules
+        smObj.unloadModule("servicemanager");
+else:
+        print"Load module failed";
+
