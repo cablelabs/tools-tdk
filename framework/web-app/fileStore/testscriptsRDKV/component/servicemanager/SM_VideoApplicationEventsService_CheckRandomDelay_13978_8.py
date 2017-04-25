d936adc45789a35d3b815020e59c40d1383c360e
smohanakumari <swapna_mohanakumari@comcast.com>
2017-04-25 11:22:06 +0000

    RDKCMF-6932 Automation for Video Application Events Service
    Reason for change: New Video Application Events Service added
    Test Procedure: Run Video Application Events Service scripts using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna.sm@tataelxsi.co.in>
    
    Change-Id: Icd73bd7d43138581efead283242550fdc9efaa7e

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/servicemanager/SM_VideoApplicationEventsService_CheckRandomDelay_13978_8.py
@@ -0,0 +1,154 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2016 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>SM_VideoApplicationEventsService_CheckRandomDelay_13978_8</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>SM_VideoApplicationEventsService_SetApplications</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Verify that randomdelay is zero when application is set with 0 max random delay</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Hybrid-1</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id></test_case_id>
+    <test_objective></test_objective>
+    <test_type></test_type>
+    <test_setup></test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used></api_or_interface_used>
+    <input_parameters></input_parameters>
+    <automation_approch></automation_approch>
+    <except_output></except_output>
+    <priority></priority>
+    <test_stub_interface></test_stub_interface>
+    <test_script></test_script>
+    <skipped></skipped>
+    <release_version></release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+import servicemanager;
+
+#Test component to be tested
+smObj = tdklib.TDKScriptingLibrary("servicemanager","1.3");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+smObj.configureTestCase(ip,port,'SM_VideoApplicationEventsService_CheckRandomDelay_13978_8');
+#Get the result of connection with test component and STB
+smLoadStatus =smObj.getLoadModuleResult();
+print "[SM LIB LOAD STATUS]  :  %s" %smLoadStatus;
+
+if "SUCCESS" in smLoadStatus.upper():
+        serviceName = "org.openrdk.videoApplicationEvents_1";
+        #Register Service
+        register = servicemanager.registerService(smObj,serviceName);
+
+        if "SUCCESS" in register:
+                #Prmitive test case which associated to this Script
+                tdkTestObj = smObj.createTestStep('SM_VideoApplicationEventsService_SetApplications');
+                maxRandDelay = 0;
+                appString = "advertisement,0,NULL";
+                count = 1;
+                expectedresult = "SUCCESS";
+                tdkTestObj.addParameter("appString",appString);
+                tdkTestObj.addParameter("count",count);
+                tdkTestObj.executeTestCase(expectedresult);
+                #Get the result of execution
+                actualresult = tdkTestObj.getResult();
+                print "[TEST EXECUTION RESULT] : %s" %actualresult;
+                if expectedresult in actualresult:
+                        print "Application set successfully";
+                        tdkTestObj.setResultStatus("SUCCESS");
+                else:
+                        print "Application set failed";
+                        tdkTestObj.setResultStatus("FAILURE");
+
+                #Prmitive test case which associated to this Script
+                tdkTestObj = smObj.createTestStep('SM_VideoApplicationEventsService_GetApplications');
+                expectedresult = "SUCCESS";
+                tdkTestObj.executeTestCase(expectedresult);
+                #Get the result of execution
+                actualresult = tdkTestObj.getResult();
+                if expectedresult in actualresult:
+                        tdkTestObj.setResultStatus("SUCCESS");
+                        print "Application retrieved successfully";
+                        resultDetails = tdkTestObj.getResultDetails();
+                        print "RESULT DETAILS: %s" %resultDetails;
+                        print "APPSTRING: %s" %appString;
+                        logpath = "/opt/TDK/logs/AgentConsole.log";
+                        filepath = tdkTestObj.transferLogs( logpath, "false" );
+                        agentlog = open(filepath,'r');
+                        status = 'FAILURE';
+                        for line in iter(agentlog):
+                                if ('Delay calculated' in line):
+                                        randDelay = int(line.split('-')[1].strip());
+                                        print "Random Delay Calulated is:%d" %randDelay;
+                                        if randDelay ==0 :
+                                                print "Random Delay correct";
+                                                status = 'SUCCESS';
+                                        else:
+                                                print "Random Delay not correct";
+                                        break;
+                        tdkTestObj.setResultStatus(status);
+
+                else:
+                        print "Application retrieval failed";
+                        tdkTestObj.setResultStatus("FAILURE");
+
+                unregister = servicemanager.unRegisterService(smObj,serviceName);
+        smObj.unloadModule("servicemanager");
+else:
+         print "Failed to load service manager module\n";
+
---
+++ framework/web-app/fileStore/testscriptsRDKV/component/servicemanager/SM_VideoApplicationEventsService_SetMultipleApplication_13978_5.py
@@ -0,0 +1,142 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2016 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>SM_VideoApplicationEventsService_SetMultipleApplication_13978_5</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>SM_VideoApplicationEventsService_SetApplications</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Checks if setting multiple applications sets the values correctly</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Hybrid-1</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id></test_case_id>
+    <test_objective></test_objective>
+    <test_type></test_type>
+    <test_setup></test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used></api_or_interface_used>
+    <input_parameters></input_parameters>
+    <automation_approch></automation_approch>
+    <except_output></except_output>
+    <priority></priority>
+    <test_stub_interface></test_stub_interface>
+    <test_script></test_script>
+    <skipped></skipped>
+    <release_version></release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+import servicemanager;
+
+#Test component to be tested
+smObj = tdklib.TDKScriptingLibrary("servicemanager","2.0");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+smObj.configureTestCase(ip,port,'SM_VideoApplicationEventsService_SetMultipleApplication_13978_5');
+#Get the result of connection with test component and STB
+smLoadStatus =smObj.getLoadModuleResult();
+print "[SM LIB LOAD STATUS]  :  %s" %smLoadStatus;
+
+if "SUCCESS" in smLoadStatus.upper():
+        serviceName = "org.openrdk.videoApplicationEvents_1";
+        #Register Service
+        register = servicemanager.registerService(smObj,serviceName);
+
+        if "SUCCESS" in register:
+                #Prmitive test case which associated to this Script
+                tdkTestObj = smObj.createTestStep('SM_VideoApplicationEventsService_SetApplications');
+                appString = "advertisement,15,NULL,buy_this1,5,NULL,buy_this2,10,NULL";
+                count = 3;
+                expectedresult = "SUCCESS";
+                tdkTestObj.addParameter("appString",appString);
+                tdkTestObj.addParameter("count",count);
+                tdkTestObj.executeTestCase(expectedresult);
+                #Get the result of execution
+                actualresult = tdkTestObj.getResult();
+                print "[TEST EXECUTION RESULT] : %s" %actualresult;
+                if expectedresult in actualresult:
+                        print "Application set successfully";
+                        tdkTestObj.setResultStatus("SUCCESS");
+                else:
+                        print "Application set failed";
+                        tdkTestObj.setResultStatus("FAILURE");
+
+                #Prmitive test case which associated to this Script
+                tdkTestObj = smObj.createTestStep('SM_VideoApplicationEventsService_GetApplications');
+                expectedresult = "SUCCESS";
+                tdkTestObj.executeTestCase(expectedresult);
+                #Get the result of execution
+                actualresult = tdkTestObj.getResult();
+                if expectedresult in actualresult:
+                        tdkTestObj.setResultStatus("SUCCESS");
+                        print "Application retrieved successfully";
+                        resultDetails = tdkTestObj.getResultDetails();
+                        print "RESULT DETAILS: %s" %resultDetails;
+                        print "APPSTRING: %s" %appString;
+                        if resultDetails == appString:
+                                print "Strings are equal";
+                        else:
+                                print "Strings are not equal";
+
+                else:
+                        print "Application retrieval failed";
+                        tdkTestObj.setResultStatus("FAILURE");
+
+                unregister = servicemanager.unRegisterService(smObj,serviceName);
+        smObj.unloadModule("servicemanager");
+else:
+         print "Failed to load service manager module\n";
+
