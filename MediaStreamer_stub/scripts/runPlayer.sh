400359de67002aa06baaaef545e158ec14000f77
sarveswaran <sarveswaran@tataelxsi.co.in>
2017-08-15 15:28:13 -0400

    RDKCMF-7603 RDKTT-1206 : Removing Apache license to RDKM license for specific components
    Reason for change: Replacing the apache header with RDKM for TDK tdkintegration scripts
    Test Procedure: using TDK Test Manager
    Risks: None
    Signed-off-by: sarveswaran <sarveswaran@tataelxsi.co.in>
    
    Change-Id: I53ce8bc31608c305a34a803f507543dac2c95cfb

--- MediaStreamer_stub/scripts/runPlayer.sh
+++ MediaStreamer_stub/scripts/runPlayer.sh
@@ -1,24 +1,13 @@
-#!/bin/sh
-##########################################################################
-# If not stated otherwise in this file or this component's Licenses.txt
-# file the following copyright and licenses apply:
-#
-# Copyright 2016 RDK Management
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-##########################################################################
-#
-# this script sets the runtime environment variables, play the url and copy the ocapri log to mediastreamer log file
+#!/bin/bash
+# ============================================================================
+# RDK MANAGEMENT, LLC CONFIDENTIAL AND PROPRIETARY
+# ============================================================================
+# This file (and its contents) are the intellectual property of RDK Management, LLC.
+# It may not be used, copied, distributed or otherwise  disclosed in whole or in
+# part without the express written permission of RDK Management, LLC.
+# ============================================================================
+# Copyright (c) 2016 RDK Management, LLC. All rights reserved.
+# ============================================================================
 
 export LD_LIBRARY_PATH=/mnt/nfs/bin/gstreamer_plugins/:/mnt/nfs/bin/rstreamer/lib:/lib:$LD_LIBRARY_PATH
 export GST_PLUGIN_PATH=/lib/gstreamer-0.10:/usr/local/lib/gstreamer-0.10:/mnt/nfs/bin/gstreamer_plugins/
