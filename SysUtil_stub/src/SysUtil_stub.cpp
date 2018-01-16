b0a9cfc893bce87ee0375b85042985e4839a766f
smohanakumari <swapna_mohanakumari@comcast.com>
2018-01-16 11:36:28 +0000

    RDKTT-1365 : Making SysUtil module generic
    Reason for change: Moving SysUtil module to TDK generic framework
    Test Procedure: Run SysUtil script using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna.sm@tataelxsi.co.in>
    
    Change-Id: I9f0fef4bfaf4463892c44a481f3f7399532645b2

---
+++ SysUtil_stub/src/SysUtil_stub.cpp
@@ -0,0 +1,483 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2016 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#include "SysUtil_stub.h"
+
+/*************************************************************************
+  Function name : SysUtilAgent::SysUtilAgent
+
+Arguments     : NULL
+
+Description   : Constructor for SysUtilAgent class
+ ***************************************************************************/
+#if 0
+SysUtilAgent::SysUtilAgent()
+{
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent Initialized\n");
+}
+#endif
+/***************************************************************************
+ *Function name : testmodulepre_requisites
+ *Descrption    : testmodulepre_requisites will  be used for setting the
+ *                pre-requisites that are necessary for this component
+ *
+ *****************************************************************************/
+std::string SysUtilAgent::testmodulepre_requisites()
+{
+	return "SUCCESS";
+}
+
+/***************************************************************************
+ *Function name : testmodulepost_requisites
+ *Descrption    : testmodulepost_requisites will be used for resetting the
+ *                pre-requisites that are set
+ *
+ *****************************************************************************/
+bool SysUtilAgent::testmodulepost_requisites()
+{
+	return true;
+}
+
+/**************************************************************************
+Function name : SysUtilAgent::initialize
+
+Arguments     : Input arguments are Version string and SysUtilAgent obj ptr
+
+Description   : Registering all the wrapper functions with the agent for using these functions in the script
+ ***************************************************************************/
+bool SysUtilAgent::initialize(IN const char* szVersion)
+{
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent Initialize----->Entry\n");
+#if 0
+	ptrAgentObj->RegisterMethod(*this,&SysUtilAgent::SysUtilAgent_GetifconfigValue, "TestMgr_GetifconfigValue");
+	ptrAgentObj->RegisterMethod(*this,&SysUtilAgent::SysUtilAgent_TouchFile, "TestMgr_TouchFile");
+	ptrAgentObj->RegisterMethod(*this,&SysUtilAgent::SysUtilAgent_GetpingValue, "TestMgr_GetpingValue");
+	ptrAgentObj->RegisterMethod(*this,&SysUtilAgent::SysUtilAgent_GetrouteInfo, "TestMgr_GetrouteInfo");
+	ptrAgentObj->RegisterMethod(*this,&SysUtilAgent::SysUtilAgent_ExecuteCmd, "TestMgr_ExecuteCmd");
+	ptrAgentObj->RegisterMethod(*this,&SysUtilAgent::SysUtilAgent_Getoutput_json_file, "TestMgr_Getoutput_json_file");
+
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent Initialize----->Exit\n");
+#endif
+	return TEST_SUCCESS;
+}
+
+/**************************************************************************
+Function name : SysUtilAgent::SysUtilAgent_GetifconfigValue
+
+Arguments     : Input arguments are json request object and json response object
+
+Description   : This method queries for the parameter requested through curl and returns the value.
+***************************************************************************/
+void SysUtilAgent::SysUtilAgent_GetifconfigValue(IN const Json::Value& req, OUT Json::Value& response)
+{
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_GetifconfigValue -->Entry\n");
+
+	string interface = req["interface"].asCString();
+	
+	FILE *fp = NULL;
+	char readRespBuff[BUFF_LENGTH] = { '\0' };
+	string popenBuff;
+	/*Frame the command  */
+	string path = IFCONFIG;
+	path.append(interface);
+
+	DEBUG_PRINT(DEBUG_TRACE, "ifconfig Request Framed: %s\n",path.c_str());
+
+	fp = popen(path.c_str(),"r");
+
+	/*Check for popen failure*/	
+	if(fp == NULL)
+	{
+		response["result"] = "FAILURE";
+		response["details"] = "popen() failure";
+		DEBUG_PRINT(DEBUG_ERROR, "popen() failure\n");
+
+		return;
+	}
+
+	/*copy the response to a buffer */
+	while(fgets(readRespBuff,sizeof(readRespBuff),fp) != NULL)
+	{
+		DEBUG_PRINT(DEBUG_TRACE, "ifconfig Response:\n");
+		cout<<readRespBuff<<endl;
+		popenBuff += readRespBuff;
+	}
+
+	pclose(fp);	
+
+	string respResult(readRespBuff);
+	DEBUG_PRINT(DEBUG_TRACE, "\n\nResponse: %s\n",popenBuff.c_str());
+	response["result"] = "SUCCESS";
+	response["details"] = popenBuff;
+	DEBUG_PRINT(DEBUG_LOG, "Execution success\n");
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_GetifconfigValue -->Exit\n");
+	return;
+
+}
+
+/**************************************************************************
+Function name : SysUtilAgent::SysUtilAgent_GetpingValue
+
+Arguments     : Input arguments are json request object and json response object
+
+Description   : This method queries for the parameter requested through curl and returns the value.
+***************************************************************************/
+void SysUtilAgent::SysUtilAgent_GetpingValue(IN const Json::Value& req, OUT Json::Value& response)
+{
+        DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_GetpingValue -->Entry\n");
+
+        FILE *fp = NULL;
+	char readRespBuff[BUFF_LENGTH] = { '\0' };
+        string address  = req["address"].asCString();
+        bool ping6enabled = req["ping6enable"].asInt();
+	string path;
+	string popenBuff;
+        if (true == ping6enabled)
+	{
+		/*Frame the command  */
+	        path = PING6;
+	}
+	else
+	{
+		/*Frame the command  */
+	        path = PING;
+	}
+
+        if (address == "CMTS")
+        {
+                /*Frame the command  */
+		path.append(CMTS);
+        }
+        else
+        {
+                /*Frame the command  */
+                path.append(address);
+        }
+
+        DEBUG_PRINT(DEBUG_TRACE, "Ping Request Framed: %s\n",path.c_str());
+
+        fp = popen(path.c_str(),"r");
+
+        /*Check for popen failure*/
+        if(fp == NULL)
+        {
+                response["result"] = "FAILURE";
+                response["details"] = "popen() failure";
+                DEBUG_PRINT(DEBUG_ERROR, "popen() failure\n");
+
+                return;
+        }
+
+        /*copy the response to a buffer */
+        while(fgets(readRespBuff,sizeof(readRespBuff),fp) != NULL)
+        {
+                DEBUG_PRINT(DEBUG_TRACE, "Ping Response:\n");
+                cout<<readRespBuff<<endl;
+		popenBuff += readRespBuff;
+        }
+
+        pclose(fp);
+
+        string respResult(readRespBuff);
+        DEBUG_PRINT(DEBUG_TRACE, "\n\nResponse: %s\n",popenBuff.c_str());
+        response["result"] = "SUCCESS";
+        response["details"] = popenBuff;
+        DEBUG_PRINT(DEBUG_LOG, "Execution success\n");
+        DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_GetpingValue -->Exit\n");
+        return;
+
+}
+
+/**************************************************************************
+Function name : SysUtilAgent::SysUtilAgent_Getrouteinfo
+
+Arguments     : Input arguments are json request object and json response object
+
+Description   : This method queries for the parameter requested through curl and returns the value.
+***************************************************************************/
+void SysUtilAgent::SysUtilAgent_GetrouteInfo(IN const Json::Value& req, OUT Json::Value& response)
+{
+        DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_GetrouteInfo -->Entry\n");
+
+        FILE *fp = NULL;
+	char readRespBuff[BUFF_LENGTH] = { '\0' };
+        bool ip6enabled = req["ip6enable"].asInt();
+	string path;
+	string popenBuff;
+
+        if (true == ip6enabled)
+	{
+		/*Frame the command  */
+	        path = IP6CMD;
+	}
+	else
+	{
+		/*Frame the command  */
+	        path = IPCMD;
+	}
+
+	/*Frame the command  */
+	path.append(ROUTE);
+
+	DEBUG_PRINT(DEBUG_TRACE, "Route Request Framed: %s\n",path.c_str());
+
+        fp = popen(path.c_str(),"r");
+
+        /*Check for popen failure*/
+        if(fp == NULL)
+        {
+                response["result"] = "FAILURE";
+                response["details"] = "popen() failure";
+                DEBUG_PRINT(DEBUG_ERROR, "popen() failure\n");
+
+                return;
+        }
+
+        /*copy the response to a buffer */
+        while(fgets(readRespBuff,sizeof(readRespBuff),fp) != NULL)
+        {
+                DEBUG_PRINT(DEBUG_TRACE, "Route Response:\n");
+                cout<<readRespBuff<<endl;
+		popenBuff += readRespBuff;
+        }
+
+        pclose(fp);
+
+        string respResult(readRespBuff);
+        DEBUG_PRINT(DEBUG_TRACE, "\n\nResponse: %s\n",popenBuff.c_str());
+        response["result"] = "SUCCESS";
+        response["details"] = popenBuff;
+        DEBUG_PRINT(DEBUG_LOG, "Execution success\n");
+        DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_GetrouteInfo -->Exit\n");
+        return;
+
+}
+
+/**************************************************************************
+Function name : SysUtilAgent::SysUtilAgent_TouchFile
+
+Arguments     : Input arguments are json request object and json response object
+
+Description   : This method queries for the parameter requested through curl and returns the value.
+***************************************************************************/
+void SysUtilAgent::SysUtilAgent_TouchFile(IN const Json::Value& req, OUT Json::Value& response)
+{
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_TouchFile -->Entry\n");
+
+	string fileinfo = req["fileinfo"].asCString();
+	
+	FILE *fp = NULL;
+	char readRespBuff[BUFF_LENGTH] = { '\0' };
+
+	/*Frame the command  */
+	string path = TOUCH;
+	path.append(fileinfo);
+
+	DEBUG_PRINT(DEBUG_TRACE, "touch Request Framed: %s\n",path.c_str());
+
+	fp = popen(path.c_str(),"r");
+
+	/*Check for popen failure*/	
+	if(fp == NULL)
+	{
+		response["result"] = "FAILURE";
+		response["details"] = "popen() failure";
+		DEBUG_PRINT(DEBUG_ERROR, "popen() failure\n");
+
+		return;
+	}
+
+	/*copy the response to a buffer */
+	while(fgets(readRespBuff,sizeof(readRespBuff),fp) != NULL)
+	{
+		DEBUG_PRINT(DEBUG_TRACE, "Touch Response:\n");
+		cout<<readRespBuff<<endl;
+	}
+
+	pclose(fp);	
+
+	string respResult(readRespBuff);
+	DEBUG_PRINT(DEBUG_TRACE, "\n\nResponse: %s\n",respResult.c_str());
+	response["result"] = "SUCCESS";
+	response["details"] = respResult;
+	DEBUG_PRINT(DEBUG_LOG, "Execution success\n");
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_TouchFile -->Exit\n");
+	return;
+
+}
+
+
+/**************************************************************************
+Function name : SysUtilAgent::SysUtilAgent_ExecuteCmd
+
+Arguments     : Input arguments are json request object and json response object
+
+Description   : This method queries for the parameter requested through curl and returns the value.
+***************************************************************************/
+void SysUtilAgent::SysUtilAgent_ExecuteCmd(IN const Json::Value& req, OUT Json::Value& response)
+{
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_ExecuteCmd -->Entry\n");
+
+	string fileinfo = req["command"].asCString();
+	
+	FILE *fp = NULL;
+	char readRespBuff[BUFF_LENGTH] = { '\0' };
+
+	/*Frame the command  */
+	string path = "";
+	path.append(fileinfo);
+
+	DEBUG_PRINT(DEBUG_TRACE, "Command Request Framed: %s\n",path.c_str());
+
+	fp = popen(path.c_str(),"r");
+
+	/*Check for popen failure*/	
+	if(fp == NULL)
+	{
+		response["result"] = "FAILURE";
+		response["details"] = "popen() failure";
+		DEBUG_PRINT(DEBUG_ERROR, "popen() failure\n");
+
+		return;
+	}
+
+	/*copy the response to a buffer */
+	while(fgets(readRespBuff,sizeof(readRespBuff),fp) != NULL)
+	{
+		DEBUG_PRINT(DEBUG_TRACE, "Command Response:\n");
+		cout<<readRespBuff<<endl;
+	}
+
+	pclose(fp);	
+
+	string respResult(readRespBuff);
+	DEBUG_PRINT(DEBUG_TRACE, "\n\nResponse: %s\n",respResult.c_str());
+	response["result"] = "SUCCESS";
+	response["details"] = respResult;
+	DEBUG_PRINT(DEBUG_LOG, "Execution success\n");
+	DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_ExecuteCmd -->Exit\n");
+	return;
+
+}
+
+
+/**************************************************************************
+Function name : SysUtilAgent::SysUtilAgent_Getoutput_json_file
+
+Arguments     : Input arguments are json request object and json response object
+
+Description   : This method queries for the parameter requested through curl and returns the value.
+***************************************************************************/
+void SysUtilAgent::SysUtilAgent_Getoutput_json_file(IN const Json::Value& req, OUT Json::Value& response)
+{
+        DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_Getoutput_json_file -->Entry\n");
+
+        FILE *fp = NULL;
+	char readRespBuff[BUFF_LENGTH] = { '\0' };
+	string path = "cat $XDISCOVERY_PATH/xdiscovery.conf|grep outputJsonFile=|grep -v \"#\"|awk -F \"=\" '{print $2}'";
+	string popenBuff;
+
+
+	DEBUG_PRINT(DEBUG_TRACE, "Extracting output.json file location Request Framed: %s\n",path.c_str());
+
+        fp = popen(path.c_str(),"r");
+
+        /*Check for popen failure*/
+        if(fp == NULL)
+        {
+                response["result"] = "FAILURE";
+                response["details"] = "popen() failure";
+                DEBUG_PRINT(DEBUG_ERROR, "popen() failure for : %s\n",path.c_str());
+
+                return;
+        }
+
+        /*copy the response to a buffer */
+        while(fgets(readRespBuff,sizeof(readRespBuff),fp) != NULL)
+        {
+		popenBuff += readRespBuff;
+        }
+
+        pclose(fp);
+
+        DEBUG_PRINT(DEBUG_TRACE, "\n\nResponse: %s\n",popenBuff.c_str());
+        response["result"] = "SUCCESS";
+        response["details"] = popenBuff;
+        response["log-path"] = popenBuff;
+        DEBUG_PRINT(DEBUG_LOG, "Execution success\n");
+        DEBUG_PRINT(DEBUG_TRACE, "SysUtilAgent_Getoutput_json_file -->Exit\n");
+        return;
+
+}
+
+/**************************************************************************
+Function Name   : CreateObject
+
+Arguments       : NULL
+
+Description     : This function is used to create a new object of the class "SysUtilAgent".
+ **************************************************************************/
+
+extern "C" SysUtilAgent* CreateObject(TcpSocketServer &ptrtcpServer)
+{
+	DEBUG_PRINT(DEBUG_TRACE, "Creating SysUtil Agent Object\n");
+
+	return new SysUtilAgent(ptrtcpServer);
+}
+
+/**************************************************************************
+Function Name   : cleanup
+
+Arguments       : NULL
+
+Description     : This function will be used to the close things cleanly.
+ **************************************************************************/
+bool SysUtilAgent::cleanup(IN const char* szVersion)
+{
+	DEBUG_PRINT(DEBUG_TRACE, "cleaningup\n");
+#if 0
+	if(NULL == ptrAgentObj)
+	{
+		return TEST_FAILURE;
+	}
+
+	ptrAgentObj->UnregisterMethod("TestMgr_GetifconfigValue");
+	ptrAgentObj->UnregisterMethod("TestMgr_GetrouteInfo");
+	ptrAgentObj->UnregisterMethod("TestMgr_TouchFile");
+	ptrAgentObj->UnregisterMethod("TestMgr_ExecuteCmd");
+	ptrAgentObj->UnregisterMethod("TestMgr_GetpingValue");
+	ptrAgentObj->UnregisterMethod("TestMgr_Getoutput_json_file");
+
+	DEBUG_PRINT(DEBUG_TRACE, "cleaningup done\n");
+
+#endif	return TEST_SUCCESS;
+}
+
+/**************************************************************************
+Function Name : DestroyObject
+
+Arguments     : Input argument is SysUtilAgent Object
+
+Description   : This function will be used to destory the SysUtilAgent object.
+ **************************************************************************/
+extern "C" void DestroyObject(SysUtilAgent *stubobj)
+{
+	DEBUG_PRINT(DEBUG_TRACE, "Destroying SysUtilAgent object\n");
+	delete stubobj;
+}
