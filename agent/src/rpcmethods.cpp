392076fb4e9da60b9a686f515b68ad4bd5dd38c5
sarveswaran <sarveswaran@tataelxsi.co.in>
2018-01-19 12:12:24 +0000

    RDKTT-1356 : Adopting IPV6 in TDK test agent
    Reason for change: Modification in jsonrpc for ipv6 adoption
    Test Procedure:Configure box with ipv6 address and run tests
    Risks: None
    
    Change-Id: I1f6e186d7ed0c25c3a0d1c7373feebefec573ef5
    Signed-off-by: sarveswaran <sarveswaran@tataelxsi.co.in>

--- agent/src/rpcmethods.cpp
+++ agent/src/rpcmethods.cpp
@@ -1449,7 +1449,7 @@ void RpcMethods::RPCGetHostStatus (const Json::Value& request, Json::Value& resp
           (strcmp ( (request["managerIP"].asCString()), "NULL") != 0) )
     {	
         /* Fetching the connected box IP address */
-          RpcMethods::sm_strBoxIP = "127.0.0.1"; // Need to revisit this
+          RpcMethods::sm_strBoxIP = go_Server.GetIP();
 
         /* Getting corresponding network interface */
         pszInterface = GetHostIPInterface (RpcMethods::sm_strBoxIP.c_str());
